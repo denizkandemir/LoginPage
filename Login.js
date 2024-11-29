4ed4e0b9332d8f69469d2d77b024bc50aab90eea
Deniz Kandemir <denizkandemir137@gmail.com>
2024-11-29 22:00:25 +0300

    remember section functions added

--- Login.js
+++ Login.js
@@ -1,4 +1,13 @@
-const users = JSON.parse(localStorage.getItem("users")) || [];
+let users = JSON.parse(localStorage.getItem("users")) || [];
+let checkKeepLogin = JSON.parse(localStorage.getItem("keepLogin")) || false;
+
+const handleKeepLogin = () => {
+  checkKeepLogin = JSON.parse(localStorage.getItem("keepLogin")) || false;
+
+  if (checkKeepLogin) {
+    window.location.assign("homepage.html");
+  }
+}
 
 const signNewMember = () => {
   const newUser = signInErrorHandling();
@@ -14,7 +23,7 @@ const signNewMember = () => {
 };
 
 const checkUser = (newUser) => {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
+    users = JSON.parse(localStorage.getItem("users")) || [];
     const isUser = users.find((user) => user.username === newUser.username); 
     const isMail = users.find((user) => user.email === newUser.email); 
 
@@ -30,8 +39,6 @@ const checkUser = (newUser) => {
           alert("This username already exist. Please try another one");
         }
     }
-
-   
 }
 
 const signInErrorHandling = () => {
@@ -58,12 +65,16 @@ const signInErrorHandling = () => {
   }
   else {
    const userId = users.length !== 0 ? users.length + 1 : 1;
+   const userAnimal = handleRememberAnimals();
+   const userNumber = handleRememberNumbers();
    const user = {
         name: nameValue,
         email:emailValue,
         username:userNameValue,
         password: passwordValue,
         id: userId,
+        selectedAnimal:userAnimal,
+        selectedNumber:userNumber,
     }
     return user;
   } 
@@ -72,7 +83,8 @@ const signInErrorHandling = () => {
 const handleLogin = () => {
   const passwordValue = document.getElementById("login-password-input").value;
   const usernameValue = document.getElementById("login-username-input").value;
-  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const loginCheckbox = document.getElementById("login-checkbox");
+  users = JSON.parse(localStorage.getItem("users")) || [];
    
   const loginUser = users.find((user) => user.username === usernameValue);
 
@@ -80,12 +92,14 @@ const handleLogin = () => {
     const chekPassword = loginUser.password === passwordValue;
     if (chekPassword) {
        alert("Login Succesfull"); 
-       window.location.assign("homepage.html")
+       const keepLogin = loginCheckbox.checked;
+       localStorage.setItem("keepLogin", JSON.stringify(keepLogin));
+       window.location.assign("homepage.html");
     } else {
         alert("Wrong Password");
     }
   } else {
-    alert("Username does not exist")
+    alert("Username does not exist");
   }
 }
 
@@ -99,3 +113,117 @@ const rotateAnimation = (side) => {
     card.style.height = "415px";
   }
 };
+
+const rememberAnimals = [
+  {
+    id:1,
+    img: "imgs/loginAnimal1",
+    name: "Dog"
+  },
+
+  {
+    id:2,
+    img: "imgs/loginAnimal2",
+    name: "Cat"
+  },
+
+  {
+    id:3,
+    img: "imgs/loginAnimal3",
+    name: "rabbit"
+  },
+
+  
+  {
+    id:3,
+    img: "imgs/loginAnimal4",
+    name: "seal"
+  },
+
+  
+  {
+    id:5,
+    img: "imgs/loginAnimal3",
+    name: "dolphin"
+  },
+
+  
+  {
+    id:6,
+    img: "imgs/loginAnimal3",
+    name: "horse"
+  },  
+];
+
+const handleRememberAnimals = () => {
+  const rememberContainer = document.getElementById("remember-container");
+  const rememberAllContainer = document.getElementById("remember-button-container");
+  let selectedAnimals = [];
+
+  for (let i = 0; i < 4; i++) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    selectedAnimals += rememberAnimals[randomNumber];
+  }
+
+  rememberContainer.innerHTML = "";
+  selectedAnimals.map((animal) => {
+    rememberContainer.innerHTML += `
+     <div class="remember-animal-container"> 
+       <img src=${animal.img} class="animal-img"/>
+       <p class="animal-p"> ${animal.name} </p>
+       <input type="radio" id="${animal.id}" name="animal-choose" class="remember-radio">
+     </div> 
+    `
+  });
+  rememberAllContainer.innerHTML = "";
+  rememberAllContainer.innerHTML = `
+   <button class="remember-button" id="remember-animal-button"> Confrim </button>
+  `
+
+  const animalButton = rememberAllContainer.getElementById("remember-animal-button");
+  let selectedAnimal;
+
+  animalButton.addEventListener(("click") , () => {
+    const radio = rememberContainer.querySelector('input[name="animal-choose"]:checked');
+    selectedAnimal = rememberAnimals.find((animal) => animal.id === radio.id);
+  });
+
+  return selectedAnimal;
+}
+
+
+const handleRememberNumbers = () => {
+  const rememberContainer = document.getElementById("remember-container");
+  const rememberAllContainer = document.getElementById("remember-button-container");
+  rememberAllContainer.innerHTML = "";
+  rememberContainer.innerHTML = "";
+
+  let randomNumbers = [];
+   
+  for (let i = 0; i < 4; i++) {
+    randomNumbers += Math.floor(Math.random() * 100) + 1;
+  }
+
+  randomNumbers.map((number) => {
+    rememberContainer.innerHTML += `
+     <div class="remember-number-container"> 
+       <img src=${number} class="animal-img"/>
+       <input type="radio" id="${number}" name="number-choose" class="remember-radio">
+     </div> 
+    `
+  });
+  rememberAllContainer.innerHTML = `
+     <button class="remember-button" id="remeber-number-button"> Confrim </button>
+  `
+  const numberButton = rememberAllContainer.getElementById("remember-number-button");
+  let selectedNumber;
+
+  numberButton.addEventListener(("click") , () => {
+    const radio = rememberContainer.querySelector('input[name="number-choose"]:checked');
+    selectedNumber = randomNumbers.find((numberl) => number === radio.id);
+  });
+
+  return selectedNumber;
+}
+
+handleKeepLogin();
