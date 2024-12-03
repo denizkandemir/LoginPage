963e256d972af5cbd1d94d7f9fc8338c5691de2a
Deniz Kandemir <denizkandemir137@gmail.com>
2024-12-03 14:35:09 +0300

    forget-password-functions-updated

--- Login.js
+++ Login.js
@@ -1,5 +1,6 @@
 let users = JSON.parse(localStorage.getItem("users")) || [];
 let checkKeepLogin = JSON.parse(localStorage.getItem("keepLogin")) || false;
+const card = document.getElementById("card-container");
 
 const handleKeepLogin = () => {
   checkKeepLogin = JSON.parse(localStorage.getItem("keepLogin")) || false;
@@ -21,6 +22,8 @@ const signNewMember = async () => {
     inputs.forEach((input) => {
       input.value = "";
     });
+    const backContainer = document.getElementById("back-container");
+    backContainer.style.height = "500px";
     rotateAnimation("front");
   }
 };
@@ -108,7 +111,6 @@ const handleLogin = () => {
 };
 
 const rotateAnimation = (side) => {
-  const card = document.getElementById("card-container");
   card.classList.toggle("change-face");
 
   if (side === "back") {
@@ -159,12 +161,17 @@ const rememberAnimals = [
 const handleRememberAnimals = () => {
   return new Promise((resolve, reject) => {
     const signUpContainer = document.getElementById("sign-up-container");
+    const backContainer = document.getElementById("back-container");
+    backContainer.style.height = "450px";
     signUpContainer.style.display = "none";
 
-    const rememberContainer = document.getElementById("remember-container");
     const rememberAllContainer = document.getElementById(
       "remember-button-container"
     );
+    rememberAllContainer.style.display = "block";
+    rememberAllContainer.innerHTML = `<div class="sign-in-remember-container" id="remember-container"></div>`;
+
+    const rememberContainer = document.getElementById("remember-container");
     let selectedAnimals = [];
     let randomIndexs = [];
 
@@ -175,19 +182,21 @@ const handleRememberAnimals = () => {
         selectedAnimals.push(rememberAnimals[randomIndex]);
       }
     }
-
+    let content = "";
     rememberContainer.innerHTML = "";
     selectedAnimals.map((animal) => {
-      rememberContainer.innerHTML += `
+      content += `
      <div class="remember-animal-container"> 
-       <img src=${animal.img} class="animal-img"/>
-       <p class="animal-p"> ${animal.name} </p>
+       <img src=${animal.img} class="animal-img" alt=${animal.name}/>
        <input type="radio" id="animal-${animal.id}" name="animal-choose" class="remember-radio">
      </div> 
     `;
     });
+    rememberContainer.innerHTML += `
+    ${content} 
+    <button class="remember-button" id="remember-animal-button"> Confrim </button>
+    `;
 
-    rememberAllContainer.innerHTML += `<button class="remember-button" id="remember-animal-button"> Confrim </button>`;
     const titleP = document.createElement("p");
     const textP = document.createElement("p");
     titleP.textContent = "Choose an Animal";
@@ -203,7 +212,7 @@ const handleRememberAnimals = () => {
       );
 
       if (radio) {
-        const selectedAnimal = rememberAnimals.find(
+        const selectedAnimal = selectedAnimals.find(
           (animal) => animal.id === parseInt(radio.id.split("-")[1])
         );
         resolve(selectedAnimal);
@@ -234,16 +243,18 @@ const handleRememberNumbers = () => {
         randomNumbers.push(randomNumber);
       }
     }
-
+   
+    let content = "";
     randomNumbers.map((number) => {
-      rememberContainer.innerHTML += `
+      content += `
      <div class="remember-number-container"> 
        <p class="select-number"> ${number} </p>
        <input type="radio" id="number-${number}" name="number-choose" class="remember-radio">
      </div> 
     `;
     });
-    rememberAllContainer.innerHTML += `
+    rememberContainer.innerHTML += `
+     ${content}
      <button class="remember-button" id="remember-number-button"> Confrim </button>
   `;
     const titleP = document.createElement("p");
@@ -266,7 +277,6 @@ const handleRememberNumbers = () => {
         );
         rememberAllContainer.style.display = "none";
         signUpContainer.style.display = "block";
-        console.log(signUpContainer);
         resolve(selectedNumber);
       } else {
         alert("No numbers selected");
@@ -276,12 +286,11 @@ const handleRememberNumbers = () => {
 };
 
 const handleForget = () => {
-  const card = document.getElementById("card-container");
+  card.style.height = "350px";
   const loginContainer = document.getElementById("login-container");
   const passwordContainer = document.getElementById("forget-password");
   loginContainer.style.display = "none";
   passwordContainer.style.display = "block";
-  card.style.height = "500px";
 
   passwordContainer.innerHTML = `
      <h1 class="card-title"> Renew Your Password </h1>
@@ -301,81 +310,168 @@ const handleForget = () => {
   `;
 };
 
-
-const checkForgetMail = async() => {
+const checkForgetMail = async () => {
   users = JSON.parse(localStorage.getItem("users")) || [];
 
   const userNameValue = document.getElementById(
     "forget-username-input"
   ).value;
   const mailValue = document.getElementById("forget-email-input").value;
-  const userInfo = users.find((user) => userNameValue === user.userName);
+  const userInfo = users.find((user) => userNameValue === user.username);
   const passwordContainer = document.getElementById("forget-password");
-  
+
   if (userInfo && userInfo.email === mailValue) {
     passwordContainer.innerHTML = `
-    <h3 class="forget-title"> Choose an Animal </h3>
-    <p class="forget-p"> Select the same animal you had selected before for sign up </p>
+    <h3 class="forget-title" id="forget-title"> Choose an Animal </h3>
+    <p class="forget-p" id="forget-p"> Select the same animal you had selected before for sign up </p>
     <div class="forget-ask-container" id="forget-ask-container"></div>
-    <button class="remember-button"  id="forget-animal-button"> Confrim </button>
   `;
- 
-  const selectedAnimal = await askAnimal();
 
-  }else {
+    const selectedAnimal = await askAnimal(userInfo);
+    const selectedNumber = await askNumber(userInfo);
+
+    if (selectedAnimal === userInfo.selectedAnimal && selectedNumber === userInfo.selectedNumber) {
+      card.style.height = "240px";
+      passwordContainer.innerHTML = `
+     <h3 class="new-password-title"> Your New Password </h3>
+     <p class="forget-p" id="forget-p"> Determine a new pasasword </p>
+     <p class="forget-input-litte-p"> Password  </p>
+     <input type="text" id="new-password-input" class="new-password-input card-input"/>
+     <div class="remember-button-container">
+       <button class="remember-button" id="new-password-button"> Confrim </button>
+      </div> 
+    `
+    const newPasswordButton = document.getElementById("new-password-button");
+
+    newPasswordButton.addEventListener(("click"),() => {
+      const newPassword = document.getElementById("new-password-input").value;
+      if (newPassword !== "") {
+        userInfo.password = newPassword;
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("Password changed succesfully");
+        const loginContainer = document.getElementById("login-container");
+        const passwordContainer = document.getElementById("forget-password");
+        loginContainer.style.display = "block";
+        passwordContainer.style.display = "none";
+        card.style.height = "415px";
+      } else {
+        alert("Please enter a valid password")
+      }
+    });
+    } else {
+      alert("User Informations are not right")
+      window.location.reload();
+    }
+  } else {
     alert("Can't Find The User");
   }
 }
 
 
-const askAnimal = () => {
+const askAnimal = (userInfo) => {
   return new Promise((resolve, reject) => {
-      const animalContainer = document.getElementById("forget-ask-container");
-      const userAnimal = userInfo.selectedAnimal;
-      let selectedAnimals = [];
-      let randomIndexs = [];
-  
-      while (selectedAnimals.length < 3) {
-        const randomIndex = Math.floor(Math.random() * rememberAnimals.length);
-        if (!randomIndexs.includes(randomIndex)) {
-          randomIndexs.push(randomIndex);
-          selectedAnimals.push(rememberAnimals[randomIndex]);
-        }
+    card.style.height = "450px";
+    const animalContainer = document.getElementById("forget-ask-container");
+    const userAnimal = userInfo.selectedAnimal;
+    console.log(userAnimal)
+    let selectedAnimals = [];
+    let randomIndexs = [];
+
+    while (selectedAnimals.length < 3) {
+      const randomIndex = Math.floor(Math.random() * rememberAnimals.length);
+      if (!randomIndexs.includes(randomIndex) && rememberAnimals[randomIndex].id !== userAnimal.id) {
+        randomIndexs.push(randomIndex);
+        selectedAnimals.push(rememberAnimals[randomIndex]);
       }
+    }
 
-      const randomIndex = Math.floor(Math.random() * (selectedAnimals.length + 1));
-      selectedAnimals.splice(randomIndex, 0, userAnimal);
+    const randomIndex = Math.floor(Math.random() * (selectedAnimals.length + 1));
+    selectedAnimals.splice(randomIndex, 0, userAnimal);
 
-      let content = "";
-      selectedAnimals.map((animal) => {
-        content = `
+    let content = "";
+    selectedAnimals.map((animal) => {
+      content += `
         <div class="remember-animal-container"> 
-          <img src=${animal.img} class="animal-img"/>
-          <p class="animal-p"> ${animal.name} </p>
+          <img src=${animal.img} class="animal-img" alt=${animal.name}/>
           <input type="radio" id="animal-${animal.id}" name="forget-animal-choose" class="remember-radio">
         </div> 
       `;
-      });
-      animalContainer.innerHTML = content;
+    });
+    animalContainer.innerHTML = `<div class="forget-animals-container"> ${content} </div> `;
+    animalContainer.innerHTML += `<div class="remember-button-container"><button class="remember-button"  id="forget-animal-button"> Confrim </button></div>`;
 
-      const forgetAnimalButton = document.getElementById("forget-animal-id");
+    const forgetAnimalButton = document.getElementById("forget-animal-button");
 
-      forgetAnimalButton.addEventListener(("click") , () => {
-        const radio = document.querySelector(
-          'input[name="animal-choose"]:checked'
+    forgetAnimalButton.addEventListener(("click"), () => {
+      const radio = document.querySelector(
+        'input[name="forget-animal-choose"]:checked'
+      );
+
+      if (radio) {
+        const selectedAnimal = selectedAnimals.find(
+          (animal) => animal.id === parseInt(radio.id.split("-")[1])
         );
-  
-        if (radio) {
-          const selectedAnimal = rememberAnimals.find(
-            (animal) => animal.id === parseInt(radio.id.split("-")[1])
-          );
-          resolve(selectedAnimal);
-        } else {
-          alert("No Animals selected");
-        }
-      });
-  
+        resolve(selectedAnimal);
+      } else {
+        alert("No Animals selected");
+      }
+    });
   });
 };
+
+const askNumber = (userInfo) => {
+  return new Promise((resolve, reject) => {
+    const numberContainer = document.getElementById("forget-ask-container");
+    const forgetTitle = document.getElementById("forget-title");
+    const forgetP = document.getElementById("forget-p");
+    card.style.height = "415px";
+    const userNumber = userInfo.selectedNumber;
+    let selectedNumbers = [];
+    let uniqueRandomNumbers = [];
+
+    while (selectedNumbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * (100 - 10 + 1)) + 10;
+      if (!uniqueRandomNumbers.includes(randomNumber) && randomNumber !== userNumber) {
+        uniqueRandomNumbers.push(randomNumber);
+        selectedNumbers.push(randomNumber);
+      }
+    }
+
+    const randomIndex = Math.floor(Math.random() * (selectedNumbers.length + 1));
+    selectedNumbers.splice(randomIndex, 0, userNumber);
+
+    forgetTitle.textContent = "Choose a Number";
+    forgetP.textContent = "Select the same number you had selected before for sign up";
+
+    let content = "";
+    selectedNumbers.map((number) => {
+      content += `
+     <div class="remember-number-container"> 
+       <p class="select-number"> ${number} </p>
+       <input type="radio" id="number-${number}" name="forget-number-choose" class="remember-radio">
+     </div> 
+    `;
+    });
+    numberContainer.innerHTML = `<div class="forget-numbers-container"> ${content} </div>`
+    numberContainer.innerHTML += `<div class="remember-button-container"> <button class="remember-button"  id="forget-number-button"> Confrim </button> </div>`;
+    const forgetNumberButton = document.getElementById("forget-number-button");
+
+    forgetNumberButton.addEventListener(("click"), () => {
+      const radio = document.querySelector(
+        'input[name="forget-number-choose"]:checked'
+      );
+
+      if (radio) {
+        const selectedNumber = selectedNumbers.find(
+          (number) => number === parseInt(radio.id.split("-")[1])
+        );
+        resolve(selectedNumber);
+      } else {
+        alert("No Numbers Selected");
+      }
+    });
+
+  });
+}
 
 handleKeepLogin();
